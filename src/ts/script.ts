985a9f20faf9cc81a8842f650cef455f00eb7e23
Ali Hourag <ali.hourag@gmail.com>
2023-05-24 15:19:37 +0200

    feat(design): complete design and responsive

---
+++ src/ts/script.ts
@@ -0,0 +1,9 @@
+import { setHeader } from "./setHTML.js";
+
+window.addEventListener("load", loadPage);
+
+
+function loadPage(): void {
+    setHeader();
+
+}
---
+++ src/ts/setHTML.ts
@@ -0,0 +1,20 @@
+
+
+export function setHeader(): void {
+    //Crear header
+
+    //llame a main
+}
+
+export function setMain(): void {
+    //Crear main cosas que son staticas
+
+}
+
+export function setTopBar(): void {
+
+}
+
+export function setCalendarContainer(): void {
+
+}
