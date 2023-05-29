9a817a7ecc11d8bdcf7aac7602d2029b37e6021d
Ali Hourag <ali.hourag@gmail.com>
2023-05-29 14:13:15 +0200

    (fix): fix isLeapYear function

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -14,7 +14,7 @@ export function setAsideHistoryOfEvents(): void {
     if (asideHistoryOfEvents === null) return;
 
     const displayBurgerBtn: string = window.getComputedStyle(burgerBtn).display;
-    (displayBurgerBtn === "none")? asideHistoryOfEvents.classList.add("show"): asideHistoryOfEvents.classList.remove("show");
+    (displayBurgerBtn === "none") ? asideHistoryOfEvents.classList.add("show") : asideHistoryOfEvents.classList.remove("show");
 
     window.addEventListener("resize", setResizedAsideHistoryOfEvents);
 }
@@ -26,34 +26,34 @@ export function setAsideHistoryOfEvents(): void {
 function setResizedAsideHistoryOfEvents(): void {
     const asideHistoryOfEvents: (HTMLElement | null) = document.querySelector(".history-events-container_aside");
     if (asideHistoryOfEvents === null) return;
-    (window.innerWidth > 1100)? asideHistoryOfEvents.classList.add("show"):asideHistoryOfEvents.classList.remove("show");
+    (window.innerWidth > 1100) ? asideHistoryOfEvents.classList.add("show") : asideHistoryOfEvents.classList.remove("show");
 }
 
 //------------------------------------------------------------------------------------------------------
 export function eventListenerChangeYear(): void {
     const getPreviousYear: (HTMLDivElement | null) = document.querySelector(".topbar-previous-year_div");
     const getNextYear: (HTMLDivElement | null) = document.querySelector(".topbar-next-year_div");
-    if(getPreviousYear === null) return;
-    if(getNextYear === null) return;
+    if (getPreviousYear === null) return;
+    if (getNextYear === null) return;
 
     getPreviousYear.addEventListener("click", changeYearClicked);
     getNextYear.addEventListener("click", changeYearClicked);
-} 
+}
 
 //---------------------------------------------------------------------------------------------------
-function changeYearClicked(this: HTMLDivElement){
+function changeYearClicked(this: HTMLDivElement) {
     const selectedYear: (HTMLHeadingElement | null) = document.querySelector("#selected-year");
-    if(selectedYear === null) return;
+    if (selectedYear === null) return;
 
-    if(this.classList.contains("topbar-previous-year_div")){
+    if (this.classList.contains("topbar-previous-year_div")) {
         selectedYear.innerText = (parseInt(selectedYear.innerText) - 1).toString();
     } else selectedYear.innerText = (parseInt(selectedYear.innerText) + 1).toString();
     setCalendar();
 }
 
-function getTotalDaysOfMonth(month: number, year: number):number {
+function getTotalDaysOfMonth(month: number, year: number): number {
     let daysFebruary: number = 28;
-    if(isLeapYear(year)){
+    if (isLeapYear(year)) {
         daysFebruary = 29;
     }
     const arrayOfTotalDays: Array<number> = [31, daysFebruary, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -61,14 +61,5 @@ function getTotalDaysOfMonth(month: number, year: number):number {
 }
 
 function isLeapYear(year: number): boolean {
-    let isLeapYearTrue: boolean = false;
-    if(year % 4 === 0){
-        if(year % 400 === 0){
-            isLeapYearTrue = true;
-        } else{
-            if(year % 100 !== 0) isLeapYearTrue = true;
-        }
-    }
-
-    return isLeapYearTrue;
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 4 === 0 && year % 100 === 0 && year % 400 === 0);
 }
